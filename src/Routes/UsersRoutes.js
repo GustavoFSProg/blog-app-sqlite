770885a2d132b7eb25dc84a3b4f3aa906bd2819a
Gustavo <gustavo.prog40@gmail.com>
2022-02-10 14:19:23 -0300

    asdfasdf

--- src/Routes/UsersRoutes.js
+++ src/Routes/UsersRoutes.js
@@ -5,7 +5,7 @@ import { Router } from 'express'
 const route = Router()
 
 const userRoutes = [
-  route.get('/user/post', userController.getAllUsers),
+  route.get('/user/get/all', userController.getAllUsers),
   route.post('/user-register', userController.registerUser),
   route.post('/user/login', userController.Login),
 ]
